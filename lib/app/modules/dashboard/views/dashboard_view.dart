eeb2e33c26709bc0a31b9f565ce053f6394774c6
candra <candraherdiansyah14@gmail.com>
2023-03-01 08:13:41 +0700

    flutter getx

---
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -0,0 +1,400 @@
+// ignore_for_file: prefer_const_constructors, non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:lottie/lottie.dart';
+
+import '../../../data/entertainment_response.dart';
+import '../../../data/headline_response.dart';
+import '../../../data/sport_reponse.dart';
+import '../../../data/technology_response.dart';
+import '../../home/views/home_view.dart';
+import '../controllers/dashboard_controller.dart';
+
+class DashboardView extends GetView<DashboardController> {
+  const DashboardView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    DashboardController controller = Get.put(DashboardController());
+    final ScrollController scrollController = ScrollController();
+    final auth = GetStorage();
+    return SafeArea(
+      child: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              await auth.erase();
+              Get.offAll(() => const HomeView());
+            },
+            backgroundColor: Colors.redAccent,
+            child: const Icon(Icons.logout_rounded),
+          ),
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(120.0),
+            child: Column(
+              children: [
+                ListTile(
+                  title: const Text(
+                    "Hallo!",
+                    textAlign: TextAlign.end,
+                  ),
+                  subtitle: Text(
+                    auth.read('full_name').toString(),
+                    textAlign: TextAlign.end,
+                  ),
+                  trailing: Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    width: 50.0,
+                    height: 50.0,
+                    child: Lottie.network(
+                      'https://gist.githubusercontent.com/olipiskandar/2095343e6b34255dcfb042166c4a3283/raw/d76e1121a2124640481edcf6e7712130304d6236/praujikom_kucing.json',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const Align(
+                  alignment: Alignment.topLeft,
+                  child: TabBar(
+                    labelColor: Colors.black,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    isScrollable: true,
+                    indicatorColor: Colors.white,
+                    tabs: [
+                      Tab(text: "Headline"),
+                      Tab(text: "Teknologi"),
+                      Tab(text: "Olahraga"),
+                      Tab(text: "Hiburan"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              headline(controller, scrollController),
+              beritaTeknologi(controller, scrollController),
+              beritaOlahraga(controller, scrollController),
+              beritaHiburan(controller, scrollController),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  FutureBuilder<SportResponse> beritaOlahraga(
+      DashboardController controller, ScrollController scrollController) {
+    return FutureBuilder<SportResponse>(
+      future: controller.getSports(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: Lottie.network(
+              'https://gist.githubusercontent.com/olipiskandar/4f08ac098c81c32ebc02c55f5b11127b/raw/6e21dc500323da795e8b61b5558748b5c7885157/loading.json',
+              repeat: true,
+              width: MediaQuery.of(context).size.width / 1,
+            ),
+          );
+        }
+        if (!snapshot.hasData) {
+          return const Center(child: Text("Tidak ada data"));
+        }
+        return ListView.builder(
+          itemCount: snapshot.data!.data!.length,
+          controller: scrollController,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.only(
+                top: 5,
+                left: 8,
+                right: 8,
+                bottom: 5,
+              ),
+              height: 110,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(
+                      snapshot.data!.data![index].urlToImage.toString(),
+                      height: 130,
+                      width: 130,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          snapshot.data!.data![index].title.toString(),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                        const SizedBox(
+                          height: 2,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                'Author : ${snapshot.data!.data![index].author}'),
+                            Text('Sumber :${snapshot.data!.data![index].name}'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  FutureBuilder<TechnologyResponse> beritaTeknologi(
+      DashboardController controller, ScrollController scrollController) {
+    return FutureBuilder<TechnologyResponse>(
+      future: controller.getTechnology(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: Lottie.network(
+              'https://gist.githubusercontent.com/olipiskandar/4f08ac098c81c32ebc02c55f5b11127b/raw/6e21dc500323da795e8b61b5558748b5c7885157/loading.json',
+              repeat: true,
+              width: MediaQuery.of(context).size.width / 1,
+            ),
+          );
+        }
+        if (!snapshot.hasData) {
+          return const Center(child: Text("Tidak ada data"));
+        }
+        return ListView.builder(
+          itemCount: snapshot.data!.data!.length,
+          controller: scrollController,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.only(
+                top: 5,
+                left: 8,
+                right: 8,
+                bottom: 5,
+              ),
+              height: 110,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(
+                      snapshot.data!.data![index].urlToImage.toString(),
+                      height: 130,
+                      width: 130,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          snapshot.data!.data![index].title.toString(),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                        const SizedBox(
+                          height: 2,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                'Author : ${snapshot.data!.data![index].author}'),
+                            Text('Sumber :${snapshot.data!.data![index].name}'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  FutureBuilder<EntertainmentResponse> beritaHiburan(
+      DashboardController controller, ScrollController scrollController) {
+    return FutureBuilder<EntertainmentResponse>(
+      future: controller.getEntertaiment(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: Lottie.network(
+              'https://gist.githubusercontent.com/olipiskandar/4f08ac098c81c32ebc02c55f5b11127b/raw/6e21dc500323da795e8b61b5558748b5c7885157/loading.json',
+              repeat: true,
+              width: MediaQuery.of(context).size.width / 1,
+            ),
+          );
+        }
+        if (!snapshot.hasData) {
+          return const Center(child: Text("Tidak ada data"));
+        }
+        return ListView.builder(
+          itemCount: snapshot.data!.data!.length,
+          controller: scrollController,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.only(
+                top: 5,
+                left: 8,
+                right: 8,
+                bottom: 5,
+              ),
+              height: 110,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(
+                      snapshot.data!.data![index].urlToImage.toString(),
+                      height: 130,
+                      width: 130,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          snapshot.data!.data![index].title.toString(),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                        const SizedBox(
+                          height: 2,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                'Author : ${snapshot.data!.data![index].author}'),
+                            Text('Sumber :${snapshot.data!.data![index].name}'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  FutureBuilder<HeadlineResponse> headline(
+      DashboardController controller, ScrollController scrollController) {
+    return FutureBuilder<HeadlineResponse>(
+      future: controller.getHeadline(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: Lottie.network(
+              'https://gist.githubusercontent.com/olipiskandar/4f08ac098c81c32ebc02c55f5b11127b/raw/6e21dc500323da795e8b61b5558748b5c7885157/loading.json',
+              repeat: true,
+              width: MediaQuery.of(context).size.width / 1,
+            ),
+          );
+        }
+        if (!snapshot.hasData) {
+          return const Center(child: Text("Tidak ada data"));
+        }
+        return ListView.builder(
+          itemCount: snapshot.data!.data!.length,
+          controller: scrollController,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.only(
+                top: 5,
+                left: 8,
+                right: 8,
+                bottom: 5,
+              ),
+              height: 110,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(
+                      snapshot.data!.data![index].urlToImage.toString(),
+                      height: 130,
+                      width: 130,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          snapshot.data!.data![index].title.toString(),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                        const SizedBox(
+                          height: 2,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                'Author : ${snapshot.data!.data![index].author}'),
+                            Text('Sumber :${snapshot.data!.data![index].name}'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,18 @@
 import 'package:flutter/material.dart';
-import 'column_widget.dart';
-import 'container_widget.dart';
-import 'latihan/latihan_satu.dart';
-import 'list_widget/list_builder_widget.dart';
-import 'list_widget/list_example.dart';
-import 'list_widget/list_widget.dart';
-import 'row_widget.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+import 'app/routes/app_pages.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Latihan Flutter",
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.indigoAccent,
-          centerTitle: true,
-          title: Text("Latihan"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(padding: EdgeInsets.only(top: 10, bottom: 10)),
-              Text("Produk Handphone Terlaris"),
-              Container(
-                margin: EdgeInsets.all(10),
-                height: 75,
-                child: ListBuilderWidget(),
-              ),
-              Padding(padding: EdgeInsets.only(top: 10)),
-              Text("Produk HP"),
-              Padding(padding: EdgeInsets.only(top: 10)),
-              Container(
-                margin: EdgeInsets.all(16),
-                height: 600,
-                child: ListProduct(),
-              )
-            ],
-          ),
-        ),
-        // Column(
-        //   children: [
-        //     RowWidget(),
-        //     Padding(
-        //       padding: EdgeInsets.only(top: 10),
-        //     ),
-        //     ColumnWidget(),
-        //     Padding(
-        //       padding: EdgeInsets.only(top: 10),
-        //     ),
-        //     ContainerWidget()
-        //   ],
-        // ),
-      ),
-    );
-  }
-}
-
-class HelloWidget extends StatelessWidget {
-  const HelloWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Hallo World",
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          color: Colors.blue,
-          backgroundColor: Colors.black12,
-        ),
-      ),
-    );
-  }
+void main() async {
+  await GetStorage.init();
+  runApp(
+    GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
 }
